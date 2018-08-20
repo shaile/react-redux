550955bde16e85773004162e52a54984aa433c12
ss251326 <ss251326@ncr.com>
2018-08-20 12:02:39 +0530

    Remove Dummy expenses

--- src/app.js
+++ src/app.js
@@ -11,20 +11,20 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' , amount:4500, createdAt:150}));
-store.dispatch(addExpense({ description: 'Gas bill', amount:1400, createdAt:1250 }));
-store.dispatch(addExpense({ description: 'Electic bill', amount:300, createdAt:1250 }));
-store.dispatch(addExpense({ description: 'Credit bill', amount:800, createdAt:1250 }));
-store.dispatch(addExpense({ description: 'Petrol bill', amount:400, createdAt:1250 }));
-// store.dispatch(setTextFilter('water'));
+// store.dispatch(addExpense({ description: 'Water bill' , amount:4500, createdAt:150}));
+// store.dispatch(addExpense({ description: 'Gas bill', amount:1400, createdAt:1250 }));
+// store.dispatch(addExpense({ description: 'Electic bill', amount:300, createdAt:1250 }));
+// store.dispatch(addExpense({ description: 'Credit bill', amount:800, createdAt:1250 }));
+// store.dispatch(addExpense({ description: 'Petrol bill', amount:400, createdAt:1250 }));
+// // store.dispatch(setTextFilter('water'));
 
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'));
-// }, 3000)
+// // setTimeout(() => {
+// //   store.dispatch(setTextFilter('bill'));
+// // }, 3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
